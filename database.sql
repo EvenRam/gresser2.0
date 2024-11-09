1cb9c68965bf0b3a73f2d101c20be34785dbe749
Justin Kahn <jdavidkahn@gmail.com>
2024-11-08 18:58:45 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -82,3 +82,16 @@ FROM (
   WHERE job_id IS NOT NULL
 ) sub
 WHERE add_employee.id = sub.id;
+
+ALTER TABLE "jobs" 
+ADD COLUMN "display_order" INTEGER;
+
+-- Initialize the display order based on existing job_id order
+UPDATE jobs
+SET display_order = sub.row_num - 1
+FROM (
+  SELECT job_id, 
+         ROW_NUMBER() OVER (ORDER BY job_id) as row_num
+  FROM jobs
+) sub
+WHERE jobs.job_id = sub.job_id;
