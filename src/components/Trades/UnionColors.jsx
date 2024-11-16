b4719a7a9fe76b5c47efd5e1038745fc21674c04
Justin Kahn <jdavidkahn@gmail.com>
2024-11-16 13:09:25 -0600

    updated database.sql and unioncolors

--- src/components/Trades/UnionColors.jsx
+++ src/components/Trades/UnionColors.jsx
@@ -4,8 +4,10 @@ const unionColors = {
   '23 - Laborers': 'black',
   '24 - Operators': 'purple',
   '25 - Carpenters': 'blue',
-  '26 - Superintendents': 'pink',
-  '27 - Shop/Trucking': 'orange'
+  '26 - Supervisors': '#FF69B4', // hot pink
+  '27 - Trucking': '#FFA500',    // orange
+  '28 - Shop': '#4169E1',        // royal blue
+  '29 - Non-Union': '#808080'    // gray
 };
 
 export default unionColors;
