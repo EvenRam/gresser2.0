576b5806202598b6620e9f3702da46bd87e8dc1e
crystal <crystalevenram@gmail.com>
2025-12-21 02:20:31 -0600

    implement two-page print layout with wrapper: - Projects print on page 1 (10 columns) - Unions print on page 2 (8 columns) - Added SchedulingLayout wrapper with print controls - Clean page breaks working - Need to fix screen layout overlap issue

--- src/components/Scheduling/SchedulingLayout.jsx
+++ src/components/Scheduling/SchedulingLayout.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Scheduling from './Scheduling';
 import Trades from '../Trades/Trades';
+import './SchedulingLayout.css'; // We'll create this
 
 const SchedulingLayout = () => {
   const isEditable = useSelector((state) => state.scheduleReducer.isEditable);
 
   return (
-    <>
-      <Scheduling />
-      <Trades isEditable={isEditable} />
-    </>
+    <div className="scheduling-layout-wrapper">
+      <div className="print-page-1">
+        <Scheduling />  {/* Projects - will print on page 1 */}
+      </div>
+      <div className="print-page-2">
+        <Trades isEditable={isEditable} />  {/* Unions - will print on page 2 */}
+      </div>
+    </div>
   );
 };
 
